175ee555c1756adc8f31cb357aac94a79ece8eb2
realsibotz <rilrealsi@gmail.com>
2022-09-05 11:15:53 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • Tesel [082239202895]
+│ • Gopay  [082239202895]
+│ • Dana  [082239202895]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6282239202895
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
